f487ae1185068b8c65725843e182a189830816ea
Waleed Umer <dev.waleedumer@gmail.com>
2026-05-11 12:24:38 +0500

    task2
    Move profile generator; add number guessing game
    
    Moved personal profile generator logic from Program.cs to Task1.cs without changes. Removed all code from Program.cs. Added a new number guessing game in Task2.cs that provides feedback and counts valid attempts.

---
+++ Task2.cs
@@ -0,0 +1,44 @@
+﻿int secretNo = 34;
+int validAttempts = 0;
+int guess = 0;
+bool isGuessed = false;
+int highThreshold = 20;
+
+do
+{
+    Console.WriteLine("Enter your guess:");
+
+    if (int.TryParse(Console.ReadLine(), out guess))
+    {
+        validAttempts++;
+
+        if (guess == secretNo)
+        {
+            isGuessed = true;
+        }
+        else if ((guess - secretNo) > highThreshold)
+        {
+            Console.WriteLine("Guess is Too High");
+        }
+        else if (guess > secretNo)
+        {
+            Console.WriteLine("Guess is High");
+        }
+        else if ((secretNo - guess) > highThreshold)
+        {
+            Console.WriteLine("Guess is Too Low");
+        }
+        else if (guess < secretNo)
+        {
+            Console.WriteLine("Guess is Low");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please Enter Valid number");
+    }
+
+}
+while (!isGuessed);
+
+Console.WriteLine($"{secretNo} guessed in {validAttempts} attempts.");
